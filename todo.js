fa0a752cd69b32ba3dddd0e8257c9801550a23ff
kiran <kiran.talapaku@gmail.com>
2015-12-14 16:20:47 -0800

    todo list

---
+++ todo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kiran on 12/13/15.
+ */
+// complete: add vendor
+// complete: add vendor address
+// complete: add vendor contact
+// complete: add vendor contact address
+// complete: get vendor
+// complete: get vendor address(es)
+// complete: get vendor contacts
+// complete: get vendor contact address(es)
+// complete: remove vendor
+// complete: remove vendor address
+// complete: remove vendor contact
+// complete: remove vendor contact address
+// todo: update vendor
+// todo: update vendor address
+// todo: update vendor contact
+// todo: update vendor contact address
