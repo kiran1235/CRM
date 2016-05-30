9b7a4cdc2eda988f96a6a54e4c65c692ab446650
Kiran Talapaku <kiran.talapaku@gmail.com>
2016-05-29 21:24:40 -0700

    Sun May 29 21:23:47 PDT 2016
    Cleanup & Refactored
    1. removed files/folder that are not required

--- www/services/location.js
+++
@@ -1,7 +0,0 @@
-app
-.service('$locationservice',['$http',function($http){
-    this.find=function(street,city,state){
-      street = street.split(' ').join('+');    
-      return $http.get('https://maps.googleapis.com/maps/api/geocode/json?address='+street+',+'+city+',+'+state);
-    }
-}]);
--- www/services/upload.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by kiran on 1/13/16.
- */
